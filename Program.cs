dfe7b15551beb814bc5abe598d0296f6cf9cf9fc
sp <sp@DESKTOP-DTROK37>
2022-07-05 20:38:22 -0300

    Agregar archivos de proyecto.

---
+++ Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ejercicio1DeQuirozMarcelaPaola
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+			double dolares;
+			double euros;
+			int pesos;
+
+			Console.Write("Ingrese la cantidad de pesos disponibles");
+			pesos = int.Parse(Console.ReadLine());
+
+			dolares = pesos / 230;
+			euros = dolares / 1.17;
+
+			Console.WriteLine($"Podrá comprar {dolares} dolares");
+
+			
+
+			Console.WriteLine($"Podrá comprar el equivalente  {euros} en euros");
+			Console.ReadLine();
+		}
+	}
+}
